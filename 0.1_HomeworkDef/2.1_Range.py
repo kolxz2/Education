2f268448442af9312ecc8325ab42c257d3771518
Kolxz2 <kolxz2001@gmail.com>
2020-09-19 22:09:45 +0300

    Correction of mistakes

---
+++ 0.1_HomeworkDef/2.1_Range.py
@@ -0,0 +1,18 @@
+while True:
+    range_low = int(input("Enter low rage: "))
+    range_high = int(input("Enter hight range: "))
+    num = int(input("Enter num to compare: "))
+    if range_low > range_high:
+        print("Error range low bigger than highr")
+    else:
+        break
+
+
+def main(num, range_low, range_high):
+    if num in range(range_low, range_high + 1):
+        print("{} in the renge between {} and {}".format(num, range_low, range_high))
+    else:
+        print("{} not in the renge between {} and {}".format(num, range_low, range_high))
+
+
+print(main(num, range_low, range_high))
---
+++ 0.1_HomeworkDef/3.1_Count_ABCDabcd.py
@@ -0,0 +1,19 @@
+list_my = input("Enter your phrase: ")
+
+
+def main(list_my):
+    trans_list = [i for i in list_my]
+    count = {"upper": 0, "lower": 0}
+    for i in trans_list:
+        if i.isupper():
+            count["upper"] += 1
+        elif i.islower():
+            count["lower"] += 1
+        else:
+            pass
+    print("Исходная строка: ", list_my)
+    print("Количество символов в нижнем регистре: ", count["lower"])
+    print("Количество символов в верхнем регистре: ", count["upper"])
+
+
+print(main(list_my))
