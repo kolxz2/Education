d5bdade37e4d81bd7f43477520c76d299e569eb2
kolxz2 <kolxz2@mail.ru>
2021-03-09 21:37:46 +0300

    Codewars Day 6

--- Codewars/6 kyu Roman Numerals Encoder.py
+++ Codewars/6 kyu Roman Numerals Encoder.py
@@ -1,13 +1,24 @@
-def solution(n):
-    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    thousand = int(n / 1000)
-    five_hundred = int((n - thousand * 1000) / 500)
-    one_hunred = int((n - thousand * 1000 - five_hundred * 500) / 100)
-    fify = int((n - thousand * 1000 - five_hundred * 500 - one_hunred * 100) / 50)
-    ten = int((n - thousand * 1000 - five_hundred * 500 - one_hunred * 100 - fify * 50) / 10)
-    five = int((n - thousand * 1000 - five_hundred * 500 - one_hunred * 100 - fify * 50 - ten * 10) / 5)
-    one = int((n - thousand * 1000 - five_hundred * 500 - one_hunred * 100 - fify * 50 - ten * 10 - five * 5) / 1)
-    return thousand, five_hundred, one_hunred, fify, ten, five, one
-
+def solution(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    n = num
+    roman_num = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return print(f"solution({n}), '{roman_num}'")
 
 print(solution(2345))
