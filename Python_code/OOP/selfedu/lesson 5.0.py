ac591584ce4700d6c4a911f55a664c5ddd64e100
Kolxz2 <kolxz2001@gmail.com>
2021-05-08 15:22:16 +0300

    return coding

---
+++ Python_code/OOP/selfedu/lesson 5.0.py
@@ -0,0 +1,23 @@
+class Point:
+    """
+    count = 0 # статическое свойство при его изменении Point.count = 10
+    во всех экземплярах значение count будет изменено
+    """
+    __count = 0
+
+    def __init__(self,y=0 ,x=0):
+        Point.__count += 1
+        # сейчас создаёл локальные свойства для экземпляров
+        self.cordx = x
+        self.cordy = y
+
+    # создали статический метод который работает только с атрибутами класса Point
+    @staticmethod
+    def getCount(): # так как метод статический, то self не нужен
+        return Point.__count
+
+
+pt = Point()
+pt2 = Point()
+Point.count = 10
+print(pt.count,pt2.getCount())
