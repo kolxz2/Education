3c3e84c3291fc0d96e9f88deae9392b252bb38ca
nik <kolxz2@mail.ru>
2021-09-07 01:21:39 +0500

    huuhgu

---
+++ Python_code/Scraping/firstWeb.py
@@ -0,0 +1,20 @@
+import requests # для гет запрсов http
+from lxml import etree
+import lxml.html
+import csv
+
+
+def parser(url):
+    api = requests.get(url)
+    tree = lxml.html.document_fromstring(api.text)
+    text_original = tree.xpath('//*[@id="click_area"]/div//*[@class="original"]/text()')
+    for i in range(len(text_original)):
+        print(text_original[i])
+
+
+def main():
+    parser("https://www.amalgama-lab.com/songs/t/tones_and_i/dance_monkey.html")
+
+
+if __name__ == "__main__":
+    main()
